5f5b1a71a6cf71d8b2d5326ca20fa97e8781e7f4
Isaac Bueno <ibueno.dev@gmail.com>
2025-08-05 12:45:43 -0300

    Criado subpasta e arquivo para erro de waring faltava navbar

---
+++ painel/components/navbar.php
@@ -0,0 +1,34 @@
+<?php
+// painel/components/navbar.php
+$user_email = $_SESSION['usuario_email'] ?? 'Usuário';
+?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Painel Fluorita</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#adminNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="adminNavbar">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Dashboard</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                        Produtos
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-dark">
+                        <li><a class="dropdown-item" href="produtos_listar.php">Listar Produtos</a></li>
+                        <li><a class="dropdown-item" href="produtos_cadastrar.php">Cadastrar Novo</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <div class="d-flex">
+                 <span class="navbar-text me-3">
+                    Logado como: <?php echo htmlspecialchars($user_email); ?>
+                </span>
+                <a href="../logout.php" class="btn btn-outline-danger">Sair</a>
+            </div>
+        </div>
+    </div>
+</nav>
